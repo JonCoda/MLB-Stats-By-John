4dde57924d7a3442a4f027153836d5dfe749a75c
JonCoda <jocronin65@gmail.com>
2025-07-08 08:10:01 -0400

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from MLBDeepDive import find_team, find_players
 from LiveScores import datetime
-pip install streamlit-autorefresh
 from streamlit_autorefresh import st_autorefresh 
-import request
+import requests
 
 # --- PAGE CONFIG ---
 st.set_page_config(
