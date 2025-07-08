8c3240d207f7076439ac10e8c5308b412b8c227d
JonCoda <jocronin65@gmail.com>
2025-07-08 11:52:32 -0400

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -6,6 +6,6 @@ app = ('webapp.html')
 def index():
     return render_template(app) 
     
-if __name__ == 'webapp.html':
+if app == 'webapp.html':
     # Use debug=False in a production environment
     app.run(debug=True, host='0.0.0.0')
