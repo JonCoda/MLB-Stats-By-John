64c7db3acb7c9aaf47f75fba36c340908aecd82b
JonCoda <jocronin65@gmail.com>
2025-07-08 11:50:24 -0400

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -4,7 +4,7 @@ app = ('webapp.html')
 
 @app.route('/')
 def index():
-    return render_template('webapp.html') 
+    return render_template(app) 
     
 if __name__ == 'flask':
     # Use debug=False in a production environment
