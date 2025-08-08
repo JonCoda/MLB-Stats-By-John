8c93ea1b1424fd783368c028c1f36f67a9d97373
JonCoda <jocronin65@gmail.com>
2025-08-08 13:41:27 -0400

    Update App.py

--- App.py
+++ App.py
@@ -121,8 +121,8 @@ if __name__ == "__main__":
                 print("Invalid input. Please enter a number.")
 
         if player_id_to_fetch:
-            display_player_stats(api, player_id_to_fetch)        url = f"{BASE_URL}/people/search?names={player_name}"
-        try:
+            display_player_stats(api, player_id_to_fetch)       
+            try:
             response = requests.get(url)
             response.raise_for_status()
             data = response.json()
